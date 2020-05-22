50a0a6f0f836458901cb2747fd13c80668fb6e9c
peterthejohnston <peter.johnston.xc@gmail.com>
2020-05-22 16:41:53 -0600

    Table::delete

--- src/table.rs
+++ src/table.rs
@@ -5,15 +5,22 @@ const MAX_LOAD: f64 = 0.75;
 fn hash(s: &str) -> usize {
     let mut hash: usize = 2166136261;
     for c in s.chars() {
-      hash ^= c as usize;
-      hash = hash.wrapping_mul(16777619);
+        hash ^= c as usize;
+        hash = hash.wrapping_mul(16777619);
     }
     hash
 }
 
 pub struct Table {
-    entries: Vec<Option<Entry>>,
+    entries: Vec<Slot>,
     count: usize,
+}
+
+#[derive(Clone)]
+enum Slot {
+    Empty,
+    Tombstone, // Marks deleted entries
+    Entry(Entry),
 }
 
 #[derive(Clone)]
@@ -22,71 +29,105 @@ struct Entry {
     value: Value,
 }
 
-impl Table {
-    pub fn new() -> Table {
+impl Default for Table {
+    fn default() -> Self {
         Table {
             entries: vec![],
             count: 0,
         }
     }
+}
 
+impl Table {
     fn grow_capacity(cap: usize) -> usize {
-        if cap < 8 { 8 } else { cap * 2 }
+        if cap < 8 {
+            8
+        } else {
+            cap * 2
+        }
     }
 
     fn adjust_capacity(&mut self, cap: usize) {
-        let mut new_entries = vec![None; cap];
+        let mut new_entries = vec![Slot::Empty; cap];
+        self.count = 0;
         for entry in &self.entries {
-            if let Some(entry) = entry {
+            if let Slot::Entry(entry) = entry {
                 let (i, _) = self.find_entry(&new_entries, &entry.key);
-                new_entries[i] = Some(entry.clone());
+                new_entries[i] = Slot::Entry(entry.clone());
+                self.count += 1;
             }
         }
         self.entries = new_entries;
     }
 
-    fn find_entry<'a>(&self, entries: &'a Vec<Option<Entry>>, key: &str)
-    -> (usize, Option<&'a Entry>)
-    {
+    fn find_entry<'a>(&self, entries: &'a Vec<Slot>, key: &str) -> (usize, &'a Slot) {
         let mut i = hash(key) % entries.len();
+        let mut last_tombstone: Option<&Slot> = None;
         loop {
-            let entry = &entries[i];
-            if entry.is_none() { return (i, None); }
-            if let Some(entry) = entry {
-                if entry.key == key {
-                    return (i, Some(entry));
+            let slot = &entries[i];
+            match slot {
+                Slot::Tombstone => last_tombstone = Some(slot),
+                Slot::Empty => match last_tombstone {
+                    None => return (i, &Slot::Empty),
+                    Some(tombstone) => return (i, tombstone),
+                },
+                Slot::Entry(entry) => {
+                    if entry.key == key {
+                        return (i, &slot);
+                    }
                 }
             }
             i = (i + 1) % entries.len();
         }
     }
 
-    pub fn insert(&mut self, key: &str, value: Value) -> bool {
+    pub fn insert(&mut self, key: &str, value: Value) -> Option<Value> {
         if (self.count + 1) as f64 > self.entries.len() as f64 * MAX_LOAD {
             let new_cap = Self::grow_capacity(self.entries.len());
             self.adjust_capacity(new_cap);
         }
 
         let (i, entry) = self.find_entry(&self.entries, &key);
-        let is_new_key = entry.is_none();
-        if is_new_key {
-            self.count += 1;
-        }
-        self.entries[i] = Some(Entry { key: key.into(), value });
-        is_new_key
+        let result = match entry {
+            Slot::Entry(entry) => Some(entry.value.clone()),
+            Slot::Tombstone => None,
+            Slot::Empty => {
+                self.count += 1;
+                None
+            }
+        };
+        self.entries[i] = Slot::Entry(Entry {
+            key: key.into(),
+            value,
+        });
+        result
     }
 
-    pub fn get(&self, key: &str) -> Option<Value> {
-        if self.count == 0 { return None };
+    pub fn get(&self, key: &str) -> Option<&Value> {
+        if self.count == 0 {
+            return None;
+        };
 
         match self.find_entry(&self.entries, key) {
-            (_, None) => None,
-            (_, Some(entry)) => Some(entry.value.clone()),
+            (_, Slot::Entry(entry)) => Some(&entry.value),
+            _ => None,
         }
     }
 
-    fn delete(&self, key: &str) {
-        // TODO
+    fn delete(&mut self, key: &str) -> bool {
+        if self.count == 0 {
+            return false;
+        }
+
+        let (i, entry) = self.find_entry(&self.entries, key);
+        match entry {
+            Slot::Entry(_) => {
+                // Place a tombstone in the entry.
+                self.entries[i] = Slot::Tombstone;
+                true
+            }
+            _ => false,
+        }
     }
 }
 
@@ -97,17 +138,34 @@ mod tests {
 
     #[test]
     fn lots_of_entries() {
-        let mut table = Table::new();
+        let mut table = Table::default();
         for i in 0..100 {
-            table.insert(&i.to_string(), Value::Number(i as f64));
+            assert_eq!(table.insert(&i.to_string(), Value::Number(i as f64)), None);
         }
         for i in 0..100 {
-            assert_eq!(table.get(&i.to_string()), Some(Value::Number(i as f64)));
+            assert_eq!(table.get(&i.to_string()), Some(&Value::Number(i as f64)));
         }
         for i in -100..0 {
             assert_eq!(table.get(&i.to_string()), None);
         }
         for i in 101..200 {
+            assert_eq!(table.get(&i.to_string()), None);
+        }
+    }
+
+    #[test]
+    fn delete_entries() {
+        let mut table = Table::default();
+        for i in 0..100 {
+            assert_eq!(table.insert(&i.to_string(), Value::Number(i as f64)), None);
+        }
+        for i in (0..100).skip(1).step_by(2) {
+            assert_eq!(table.delete(&i.to_string()), true);
+        }
+        for i in (0..100).step_by(2) {
+            assert_eq!(table.get(&i.to_string()), Some(&Value::Number(i as f64)));
+        }
+        for i in (0..100).skip(1).step_by(2) {
             assert_eq!(table.get(&i.to_string()), None);
         }
     }
